e64d4cad792a2f24d3e592dbbff59492017a9d60
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-10-17 20:49:53 +0200

    fix: adapt to latest db-lib

--- src/query.util.ts
+++ src/query.util.ts
@@ -15,13 +15,13 @@ export function dbQueryToFirestoreQuery<ROW extends ObjectWithId>(
   // filter
   // eslint-disable-next-line unicorn/no-array-reduce, unicorn/prefer-object-from-entries
   let q = dbQuery._filters.reduce((q, f) => {
-    return q.where(f.name, OP_MAP[f.op] || (f.op as WhereFilterOp), f.val)
+    return q.where(f.name as string, OP_MAP[f.op] || (f.op as WhereFilterOp), f.val)
   }, emptyQuery)
 
   // order
   // eslint-disable-next-line unicorn/no-array-reduce, unicorn/prefer-object-from-entries
   q = dbQuery._orders.reduce((q, ord) => {
-    return q.orderBy(ord.name, ord.descending ? 'desc' : 'asc')
+    return q.orderBy(ord.name as string, ord.descending ? 'desc' : 'asc')
   }, q)
 
   // limit
@@ -30,7 +30,7 @@ export function dbQueryToFirestoreQuery<ROW extends ObjectWithId>(
   // selectedFields
   if (dbQuery._selectedFieldNames) {
     // todo: check if at least id / __key__ is required to be set
-    q = q.select(...dbQuery._selectedFieldNames)
+    q = q.select(...(dbQuery._selectedFieldNames as string[]))
   }
 
   return q
